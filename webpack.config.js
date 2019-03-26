0ee45c69dccb5c98d3558c5c5dcb105322642520
ravivalluri <ravitejavalluri@gmail.com>
2019-03-26 11:25:02 -0500

    Googler Cloud Updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const PATHS = {
 
-    app: path.resolve(__dirname,'app'),
+  app: path.resolve(__dirname, 'app'),
 
-    build: path.resolve(__dirname,'build')
+  build: path.resolve(__dirname, 'build')
 
 };
 
@@ -12,98 +12,98 @@ module.exports = {
 
   entry: {
 
-     app: PATHS.app + "/index.js"
+    app: PATHS.app + "/index.js"
 
   },
 
   output: {
 
-     path: PATHS.build,
+    path: PATHS.build,
 
-     filename: 'bundle.js'
+    filename: 'bundle.js'
 
-},
+  },
 
-resolve: {
-    
-          alias: {
-    
-            'vue': 'vue/dist/vue.min.js'
-    
-          }
-    
-},
+  resolve: {
 
-devServer: {
-        
-    host: '127.0.0.1',
-        
-    port: 4040,
-        
-},
+    alias: {
 
-module: {
-    
-      loaders: [
-    
-      {
-    
-        test: /\.css$/,
-    
-        loaders: ['style-loader','css-loader']
-    
-      },
-    
-      {
-    
-        test: /\.png$/,
-    
-        loader: "url-loader?limit=100000"
-    
-      },
-    
-      {
-    
-         test: /\.jpg$/,
-    
-         loader: "file-loader"
-    
-      },
-    
-     {
-    
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-    
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-    
-      },
-    
-      {
-    
-         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-    
-         loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-    
-      },
-    
-      {
-    
-         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-    
-         loader: 'file-loader'
-    
-      },
-    
-      {
-    
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-    
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-    
-      }
-    
-    ]
-    
+      'vue': 'vue/dist/vue.min.js'
+
     }
+
+  },
+
+  devServer: {
+
+    host: 'localhost',
+
+    port: 8080,
+
+  },
+
+  module: {
+
+    loaders: [
+
+      {
+
+        test: /\.css$/,
+
+        loaders: ['style-loader', 'css-loader']
+
+      },
+
+      {
+
+        test: /\.png$/,
+
+        loader: "url-loader?limit=100000"
+
+      },
+
+      {
+
+        test: /\.jpg$/,
+
+        loader: "file-loader"
+
+      },
+
+      {
+
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+
+      },
+
+      {
+
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+
+      },
+
+      {
+
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+
+        loader: 'file-loader'
+
+      },
+
+      {
+
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+
+      }
+
+    ]
+
+  }
 
 };
